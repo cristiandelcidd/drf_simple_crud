f6242af8e1b5c7bc79aa76b992488dfe5e55807c
Cristian Delcid <cristian_delcid10@yahoo.com>
2023-03-26 20:22:57 -0600

    SECRET_KEY updated

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1n&l^9pvn5#7&z*$gvnb9!!0g80n#y8v&wl(d41plpo(3!ift'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
